b1f2b5e4a0bcd71ec97ea37247b9dbe0c393ad9e
Aryan Gupta <agupta29_be20@thapar.edu>
2023-02-26 00:18:53 +0530

    initial commit

---
+++ include/varible_def.h
@@ -0,0 +1,16 @@
+#ifndef VARIBLE_DEF_H
+#define VARIBLE_DEF_H
+
+// CAN bus varibales
+#define RPM_PKT_ID 218099784
+#define BATT_PKT_ID 218101064
+#define CAN_FREQ  1000E3
+
+int MSB=0,LSB=0;
+int tMSB=0,tLSB=0;
+unsigned long RPM;
+float temp;
+
+
+
+#endif
