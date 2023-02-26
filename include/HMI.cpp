6ff3c4a40d1752c01b44afb2204744f1c7d1e1b1
Aryan Gupta <agupta29_be20@thapar.edu>
2023-02-27 00:13:04 +0530

    added hmi

---
+++ include/HMI.cpp
@@ -0,0 +1,74 @@
+#include <varible_def.h>
+#include <Arduino.h>
+
+void HMI_print(int placeholder, unsigned long value){
+    Serial2.print("t");
+    Serial2.print(placeholder);
+    Serial2.print(".");
+    Serial2.print("txt=");
+    Serial2.print("\"");
+    Serial2.print(value);
+    Serial2.print("\"");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+
+}
+
+void HMI_print(int placeholder, float value){
+    Serial2.print("t");
+    Serial2.print(placeholder);
+    Serial2.print(".");
+    Serial2.print("txt=");
+    Serial2.print("\"");
+    Serial2.print(value);
+    Serial2.print("\"");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+
+}
+
+void hmiCANRed(){
+    Serial2.print("r1.pco=");       
+    Serial2.print("63488");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.print("r1.bco=");       
+    Serial2.print("63488");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+
+    Serial2.print("t1");           //63488 Red colour
+    Serial2.print(".");
+    Serial2.print("pco");
+    Serial2.print("=");
+    Serial2.print("63488");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    
+}
+
+void hmiCANGreen(){
+    Serial2.print("r1.pco=");       
+    Serial2.print("1024");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.print("r1.bco=");       
+    Serial2.print("1024");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+
+    Serial2.print("t1");          //1024 Green colour
+    Serial2.print(".");
+    Serial2.print("pco");
+    Serial2.print("=");
+    Serial2.print("1024");
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,34 @@
 
 //Variables and function
 #include <varible_def.h>
-
 #include <can_bus.cpp>
-
+#include <HMI.cpp>
 
 
 
 void setup(){
   Serial.begin(115200);
-
-  if (!CAN.begin(CAN_FREQ)) {
-    //hmi_red
+  Serial2.begin(9600);
   }
-}
 
 
 
 
 void loop(){
 
-CAN_get_data(&RPM,&temp,&volts);
-  
- 
+if (CAN.begin(CAN_FREQ)) {
+  hmiCANGreen(); // CAN connected 
+  CAN_get_data(&RPM,&temp,&volts);
+  HMI_print(4,RPM);
+  HMI_print(5,temp);
+  HMI_print(10,volts);
+}
+
+else{
+  hmiCANRed(); //CAN failed 
+}
+
+
+
+
 }
