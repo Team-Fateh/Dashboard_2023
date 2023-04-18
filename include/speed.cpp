4d5530b2dae6290427aed09a31df644441b75c23
JobanpreetBrar <jbrar2_be21@thapar.edu>
2023-04-18 18:40:17 +0530

    Added additional comments regarding calculation of speed

--- include/speed.cpp
+++ include/speed.cpp
@@ -15,8 +15,8 @@ void SpeedCount(int period)
 {
   if(millis()-lastTime<period)
   return;
-  Speed=((totalCounts*5032.832)/period)/slits;   
-  lastTime=millis();
+  Speed=((totalCounts*5032.832)/period)/slits;    //Speed=((totalCounts/slits)*(pi*(Wheel dia)/100)*3.6)/(period/1000) 
+  lastTime=millis();                              // where wheel dia=44.5 cm ,
   totalCounts=0;
 }
 
